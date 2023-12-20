148743e6cfdd9aa8e64ecc868671620f4fdd745c
Russ Biggs <russbiggs@gmail.com>
2023-12-20 06:58:52 -0700

    lists tables

--- openaqdb/tables/lists.sql
+++ openaqdb/tables/lists.sql
@@ -7,8 +7,10 @@ CREATE TYPE visibility_type AS ENUM (
 );
 
 
+-- users_id is the single lists owner
 CREATE TABLE IF NOT EXISTS lists (
     lists_id int PRIMARY KEY DEFAULT nextval('lists_sq')
+    , users_id int NOT NULL REFERENCES users
     , label text NOT NULL
     , description text
     , visibility visibility_type NOT NULL DEFAULT 'private'
@@ -21,24 +23,23 @@ CREATE TABLE IF NOT EXISTS sensor_nodes_list (
     sensor_nodes_lists_id int PRIMARY KEY DEFAULT nextval('sensor_nodes_lists_sq')
     , sensor_nodes_id int NOT NULL REFERENCES sensor_nodes
     , lists_id int NOT NULL REFERENCES lists
-    , notes text NOT NULL
+    , description text NOT NULL
     , UNIQUE(sensor_nodes_id, lists_id)
 );
 
 
 CREATE TYPE role_type AS ENUM (
-    'owner'
     , 'editor'
     , 'viewer'
 );
 
 CREATE SEQUENCE IF NOT EXISTS users_lists_sq START 1;
 
--- users can have multiple lists 
+-- allows non-owners to be added to lists 
 CREATE TABLE IF NOT EXISTS users_lists (
     users_lists_id int PRIMARY KEY DEFAULT nextval('users_lists_sq')
     , users_id int NOT NULL REFERENCES users
     , lists_id int NOT NULL REFERENCES lists
-    , role role_type NOT NULL default 'owner'
+    , role role_type NOT NULL default 'viewer'
     , UNIQUE(users_id, lists_id)
 );
